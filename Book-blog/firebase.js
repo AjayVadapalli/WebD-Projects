b9dfa2b1e37b8fe4f3cf60f8c3afaa136a177534
“AjayVadapalli” <vajaysivasurya@gmail.com>
2025-03-09 23:49:45 +0530

    change

--- Book-blog/firebase.js
+++ Book-blog/firebase.js
@@ -3,15 +3,15 @@ require("dotenv").config();
 
 const serviceAccount = {
   type: process.env.FB_TYPE,
-  project_id: process.env.FB_PROJECT_ID,
-  private_key_id: process.env.FB_PRIVATE_KEY_ID,
-  private_key: process.env.FB_PRIVATE_KEY?.replace(/\\n/g, '\n'),
-  client_email: process.env.FB_CLIENT_EMAIL,
-  client_id: process.env.FB_CLIENT_ID,
-  auth_uri: process.env.FB_AUTH_URI,
-  token_uri: process.env.FB_TOKEN_URI,
-  auth_provider_x509_cert_url: process.env.FB_AUTH_PROVIDER,
-  client_x509_cert_url: process.env.FB_CLIENT_CERT
+    projectId: process.env.FB_PROJECT_ID,
+    privateKeyId: process.env.FB_PRIVATE_KEY_ID,
+    privateKey: process.env.FB_PRIVATE_KEY.replace(/\\n/g, '\n'),
+    clientEmail: process.env.FB_CLIENT_EMAIL,
+    clientId: process.env.FB_CLIENT_ID,
+    authUri: process.env.FB_AUTH_URI,
+    tokenUri: process.env.FB_TOKEN_URI,
+    authProviderX509CertUrl: process.env.FB_AUTH_PROVIDER,
+    clientC509CertUrl: process.env.FB_CLIENT_CERT
 };
 
 admin.initializeApp({
