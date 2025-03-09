5f5c4057cf9e6899b1d45ce1fda874bc86c4cac4
“AjayVadapalli” <vajaysivasurya@gmail.com>
2025-03-09 23:53:04 +0530

    change

--- Book-blog/firebase.js
+++ Book-blog/firebase.js
@@ -5,7 +5,7 @@ const serviceAccount = {
   type: process.env.FB_TYPE,
     projectId: process.env.FB_PROJECT_ID,
     privateKeyId: process.env.FB_PRIVATE_KEY_ID,
-    privateKey: process.env.FB_PRIVATE_KEY.replace(/\\n/g, '\n'),
+    privateKey: process.env.FB_PRIVATE_KEY ? process.env.FB_PRIVATE_KEY.replace(/\\n/g, '\n') : undefined,
     clientEmail: process.env.FB_CLIENT_EMAIL,
     clientId: process.env.FB_CLIENT_ID,
     authUri: process.env.FB_AUTH_URI,
